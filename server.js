fd3bb015806e4966d852076abe6dcfa122c3bd73
mazoongraphiccard-jpg <mazoongraphiccard@gmail.com>
2026-06-30 12:11:10 +0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,4 +1,34 @@
-function showMessage() {
-  document.getElementById("message").innerText =
-    "🎉 الموقع يعمل بنجاح على GitHub Pages!";
+// 🔥 ضع إعدادات Firebase الخاصة بك هنا
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  databaseURL: "https://YOUR_PROJECT.firebaseio.com",
+  projectId: "YOUR_PROJECT",
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+
+let name = prompt("اكتب اسمك الوهمي:");
+
+function send() {
+  const msg = document.getElementById("msg").value;
+
+  db.ref("messages").push({
+    name: name,
+    text: msg
+  });
+
+  document.getElementById("msg").value = "";
 }
+
+// عرض الرسائل
+db.ref("messages").on("child_added", function(snapshot) {
+  const data = snapshot.val();
+
+  const div = document.createElement("div");
+  div.className = "msg";
+  div.innerText = data.name + ": " + data.text;
+
+  document.getElementById("messages").appendChild(div);
+});
